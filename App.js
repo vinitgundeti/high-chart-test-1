d27e6dadb0c776d39246f5dc8887c44653dc3036
Vinit Gundeti <vinit@glideinvest.com>
2022-06-17 15:25:13 +0530

    highchart render

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
+import HighchartsReactNative from '@highcharts/highcharts-react-native';
 import {
   SafeAreaView,
   ScrollView,
@@ -59,6 +60,144 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const maturityCalculation = (
+    rate = 0,
+    tenure = 0,
+    sipAmount = 0,
+    lumpsum = 0,
+  ) => {
+    tenure *= 12;
+    rate /= 100;
+    const mRor = (1 + rate) ** (1 / 12) - 1;
+    let targetAmount =
+      (sipAmount * ((1 + mRor) ** tenure - 1) * (1 + mRor)) / mRor;
+    if (lumpsum) {
+      targetAmount += calculaterMaturityLumpsum(
+        lumpsum,
+        tenure / 12,
+        rate * 100,
+      );
+    }
+    return Math.round(targetAmount);
+  };
+
+  const calculaterMaturityLumpsum = (lumpsumAmount, tenure, annualReturn) =>
+    lumpsumAmount * (1 + annualReturn / 100) ** tenure;
+
+  const handleSipMaturityValue = (rate, tenure) => {
+    let finalValue;
+    if (1000) {
+      finalValue = maturityCalculation(rate, tenure, 1000, 5000);
+    } else {
+      finalValue = calculaterMaturityLumpsum(5000, tenure, rate);
+    }
+    // return Math.round(finalValue);
+    return Math.round(finalValue / 100) * 100;
+  };
+  let savingsReturnsInPercentage = 20;
+  let fdReturnsInPercentage = 30;
+  let expectedReturnsInPercentage = 40;
+  let tenureInYears = 10;
+  const xAxisData = [
+    `Savings Account <br/> ${
+      handleSipMaturityValue(savingsReturnsInPercentage, tenureInYears) || 0
+    }`,
+    `Fixed Deposit <br/> ${
+      handleSipMaturityValue(fdReturnsInPercentage, tenureInYears) || 0
+    }`,
+    `This Fund* <br/> ${
+      handleSipMaturityValue(expectedReturnsInPercentage, tenureInYears) || 0
+    }`,
+  ];
+
+  const chartOptions = {
+    chart: {
+      type: 'column',
+      style: {
+        fontFamily: "font-family: 'Work Sans', sans-serif;",
+      },
+    },
+    label: {
+      style: {
+        fontFamily: 'Mark-Bold',
+      },
+    },
+    credits: {
+      enabled: false,
+    },
+    legend: {
+      enabled: false,
+    },
+    title: {
+      text: '',
+    },
+    subtitle: {
+      text: null,
+    },
+    xAxis: {
+      categories: xAxisData,
+      crosshair: false,
+      gridLineColor: '#fff',
+      lineWidth: 0,
+      minorGridLineWidth: 0,
+      lineColor: 'transparent',
+      minorTickLength: 0,
+      tickLength: 0,
+      labels: {
+        style: {
+          color: '#ccc',
+          fontSize: 12,
+          fontFamily: 'Work Sans',
+        },
+      },
+    },
+    yAxis: {
+      visible: false,
+      title: {
+        text: '',
+      },
+      gridLineColor: '#fff',
+    },
+    tooltip: {
+      headerFormat: null,
+      pointFormat:
+        '<div style="display:flex;align-items:center;font-family:Work Sans"><div style="background-color:{series.color};border-radius:50%;width:8;height:8;margin-right:3"></div><b style="margin-left:3">₹ {point.y}</b><br/></div>',
+      shared: true,
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+        borderRadius: 5,
+      },
+    },
+    series: [
+      {
+        name: ' ',
+        data: [
+          {
+            y: handleSipMaturityValue(
+              savingsReturnsInPercentage,
+              tenureInYears,
+            ),
+            color: 'blue',
+          },
+          {
+            y: handleSipMaturityValue(fdReturnsInPercentage, tenureInYears),
+            color: 'blue',
+          },
+          {
+            y: handleSipMaturityValue(
+              expectedReturnsInPercentage,
+              tenureInYears,
+            ),
+            color: 'skyblue',
+          },
+        ],
+      },
+    ],
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -84,6 +223,16 @@ const App: () => Node = () => {
             Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
+          <HighchartsReactNative
+            styles={{
+              marginTop: 30,
+              backgroundColor: '#fff',
+              justifyContent: 'center',
+              paddingLeft: 0,
+              height: 250,
+            }}
+            options={chartOptions}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
