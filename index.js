4939ba6890e350edee1f7013ee55f114648c144f
Vinit Gundeti <vinit@glideinvest.com>
2022-06-17 18:14:57 +0530

    deleted files

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
-/**
- * @format
- */
+import React from 'react';
+import {StyleSheet, View, Button} from 'react-native';
+import HighchartsReactNative from '@highcharts/highcharts-react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const modules = [
+  //'solid-gauge'
+];
 
-AppRegistry.registerComponent(appName, () => App);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chartOptions: {
+        chart: {
+          events: {
+            load() {
+              alert(window.Highcharts.version);
+            },
+          },
+        },
+        title: {
+          text: 'Default title',
+        },
+        series: [
+          {
+            data: [1, 3, 2],
+          },
+        ],
+      },
+    };
+  }
+
+  chartUpdate() {
+    this.setState({
+      chartOptions: {
+        chart: {
+          type: 'column',
+        },
+        title: {
+          text: 'Updated chart',
+        },
+      },
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <HighchartsReactNative
+          //useCDN={true}
+          styles={styles.container}
+          options={this.state.chartOptions}
+          devPath={'192.168.0.1:12345'}
+          //useSSL={true}
+          modules={modules}
+        />
+        <Button
+          onPress={this.chartUpdate.bind(this)}
+          style={styles.button}
+          title="Chart update"
+          color="#000"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+  button: {
+    justifyContent: 'center',
+  },
+});
